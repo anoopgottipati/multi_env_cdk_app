a992e5b64a8159a667190852a07563ab35f7010a
anoopgottipati <anoopgusgmail.com>
2025-04-04 13:39:54 -0400

    Initial commit

---
+++ test/multi_env_cdk_app.test.ts
@@ -0,0 +1,17 @@
+// import { Template } from '@aws-cdk/assertions';
+// import * as cdk from '@aws-cdk/core';
+// import * as MultiEnvCdkApp from '../lib/multi_env_cdk_app-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/multi_env_cdk_app-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new MultiEnvCdkApp.MultiEnvCdkAppStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
